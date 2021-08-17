844d23ba535ca2fc8b14044d1399d522d3101ab1
hasanali8334 <hasanali83@gmail.com>
2021-08-17 09:48:52 +0300

    f

---
+++ client/pages/blog/_slug.vue
@@ -0,0 +1,32 @@
+<template>
+  <article>
+     <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <img :src="article.img" :alt="article.alt" />
+    
+
+    <nuxt-content :document="article" />
+  </article>
+</template>
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const article = await $content('articles', params.slug).fetch()
+
+      return { article }
+    }
+  }
+</script>
+<style>
+  .nuxt-content h2 {
+    font-weight: bold;
+    font-size: 28px;
+  }
+  .nuxt-content h3 {
+    font-weight: bold;
+    font-size: 22px;
+  }
+  .nuxt-content p {
+    margin-bottom: 20px;
+  }
+</style>
